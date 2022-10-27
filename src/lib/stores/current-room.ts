6276ce028f9bea3a5690a914aaff98e52310562a
Antonio Sarcevic <sarcevicantonio@gmail.com>
2022-10-27 18:04:41 +0200

    select a room by default if none found

--- src/lib/stores/current-room.ts
+++ src/lib/stores/current-room.ts
@@ -1,5 +1,6 @@
 import { browser } from '$app/environment';
-import { writable } from 'svelte/store';
+import { get, writable } from 'svelte/store';
+import { rooms } from './rooms';
 
 const LS_KEY = 'current_room';
 
@@ -9,5 +10,11 @@ function set(val: string) {
 	localStorage.setItem(LS_KEY, val);
 	storeSet(val);
 }
+
+rooms.subscribe((rooms) => {
+	if (rooms.size === 0) return;
+	if (rooms.get(get({ subscribe }))) return;
+	set(rooms.entries().next().value[0]);
+});
 
 export const current_room = { subscribe, set };
