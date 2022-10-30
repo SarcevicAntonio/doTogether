db8dad1f16f9c2ceb02ac2ca5c76d44f12b75969
Antonio Sarcevic <sarcevicantonio@gmail.com>
2022-10-30 18:38:52 +0100

    respect local storage item

--- src/lib/stores/current_list.ts
+++ src/lib/stores/current_list.ts
@@ -17,10 +17,17 @@ function select_next_list_id(
 ): string {
 	// no tasks lists
 	if ($task_list_map.size === 0) return $current_list_id;
-	// no (fitting) current_list
+
+	const local_store_value = localStorage.getItem(LS_KEY);
+	if (local_store_value && $task_list_map.get(local_store_value)) {
+		return local_store_value;
+	}
+
 	if ($task_list_map.get($current_list_id)) return $current_list_id;
+
+	// no (fitting) current_list
 	const next_list_id = $task_list_map.entries().next().value[0];
-	set_current_list_id(next_list_id);
+
 	return next_list_id;
 }
 
@@ -29,6 +36,7 @@ export const current_list = derived(
 	([$task_list_map, $current_list_id]) => {
 		if (!$task_list_map || !$current_list_id) return;
 		$current_list_id = select_next_list_id($task_list_map, $current_list_id);
+		current_list_id.set($current_list_id);
 		return $task_list_map.get($current_list_id);
 	}
 );
