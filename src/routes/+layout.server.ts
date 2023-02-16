bd90740bdf9d8c2629b5a9e0c7a07905989a4832
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-02-16 17:46:39 +0100

    SSR tasklist :ooo

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,20 @@
+import { dbADMIN } from '$lib/firebase.server';
 import type { LayoutServerLoad } from './$types';
 
 export const load: LayoutServerLoad = async ({ locals, depends }) => {
 	depends('data:user');
-	return { user: locals.user };
+	if (!locals.user) {
+		return { user: locals.user };
+	}
+
+	const ref_keychain = dbADMIN.ref('/members/' + locals.user.id);
+	const keychain = await ref_keychain.get().then((d) => d.val());
+
+	const list_promises = [];
+	for (const list_id of Object.keys(keychain)) {
+		const ref_list = dbADMIN.ref('/lists/' + list_id);
+		list_promises.push(ref_list.get().then((d) => ({ id: list_id, ...d.val() })));
+	}
+
+	return { user: locals.user, task_list: Promise.all(list_promises) };
 };
--- src/routes/[list_id]/+page.svelte
+++ src/routes/[list_id]/+page.svelte
@@ -1,22 +1,24 @@
 <script lang="ts">
-	import { browser } from '$app/environment';
 	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import { db } from '$lib/firebase';
-	import { delete_list, task_list_map } from '$lib/stores/task_lists';
+	import { delete_list } from '$lib/stores/task_lists';
 	import TaskList from '$lib/TaskList.svelte';
 	import TaskListSelector from '$lib/TaskListSelector.svelte';
 	import { ref, set } from 'firebase/database';
 	import IcRoundFolderOff from '~icons/ic/round-folder-off';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
 
 	$: list_path = `/lists/${$page.params.list_id}`;
 	$: list_ref = ref(db, list_path);
 	$: item_ref = ref(db, list_path + '/tasks');
-	$: current_list = $task_list_map.get($page.params.list_id);
-	$: browser && localStorage.setItem('current_list', $page.params.list_id);
+
+	$: current_list = data.task_list?.find((l) => l.id === $page.params.list_id);
 </script>
 
-<TaskListSelector />
+<TaskListSelector task_list={data.task_list} />
 
 {#if current_list}
 	<TaskList
