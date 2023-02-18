f34bc9b83a543aae1c205e3653854a43e24850ac
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-02-18 19:05:14 +0100

    fix

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -4,5 +4,6 @@ import type { PageServerLoad } from './$types'
 export const load: PageServerLoad = async ({ locals, parent }) => {
 	const { task_list } = await parent()
 	if (!task_list) return {}
+	// find out "last list" maybe from cookie?
 	throw redirect(302, '/' + task_list[0].id)
 }
