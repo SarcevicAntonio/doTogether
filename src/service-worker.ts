b1876d5f229f8aeb6e6e182388a1b2be8fb3f2ff
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-02-22 16:59:49 +0100

    remove '/' caching

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -3,12 +3,7 @@
 // https://www.sarcevic.dev/offline-first-installable-pwa-sveltekit-workbox-precaching
 import { build, files, prerendered, version } from '$service-worker'
 import { precacheAndRoute } from 'workbox-precaching'
-const precache_list = [
-	'/', // Attention: might not be ideal for your use case - read more below.
-	...build,
-	...files,
-	...prerendered
-].map((s) => ({
+const precache_list = [...build, ...files, ...prerendered].map((s) => ({
 	url: s,
 	revision: version
 }))
