0b299facb367b5a89982629207da8000597a7168
Antonio Sarcevic <sarcevicantonio@gmail.com>
2023-02-22 16:28:24 +0100

    remember last list in cookie

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,8 @@
 import { redirect } from '@sveltejs/kit'
 import type { PageServerLoad } from './$types'
 
-export const load: PageServerLoad = async ({ locals, parent }) => {
+export const load: PageServerLoad = async ({ parent, cookies }) => {
 	const { task_list } = await parent()
 	if (!task_list) return {}
-	// find out "last list" maybe from cookie?
-	throw redirect(302, '/' + task_list[0].id)
+	throw redirect(302, '/' + cookies.get('last_list_id') || task_list[0].id)
 }
