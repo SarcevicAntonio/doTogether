36062abb1a3720c5b0e5fd20633f4b98358ae396
Antonio Sarcevic <sarcevicantonio@gmail.com>
2025-10-29 16:41:12 +0100

    fix new users lol

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -8,11 +8,11 @@ export const load: LayoutServerLoad = async ({ locals, depends }) => {
 		return { user: locals.user }
 	}
 	const keychain_ref = server_db.ref('/members/' + locals.user.id)
-	const keychain = await keychain_ref.get().then((d) => d.val() as Record<string, string>)
+	const keychain = (await keychain_ref.get().then((d) => d.val() as Record<string, string>)) || {}
 	const list_promises = []
 	for (const list_id of Object.keys(keychain)) {
 		const list_ref = server_db.ref('/lists/' + list_id)
-		list_promises.push(list_ref.get().then((d) => ({ id: list_id, ...d.val() } as Task_List)))
+		list_promises.push(list_ref.get().then((d) => ({ id: list_id, ...d.val() }) as Task_List))
 	}
 	const all_lists = await Promise.all(list_promises)
 	const task_list: Task_List[] = []
