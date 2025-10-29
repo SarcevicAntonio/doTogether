1f3388f5411a866ef7709df661aaf05c7f52fece
Antonio Sarcevic <sarcevicantonio@gmail.com>
2025-10-29 16:45:46 +0100

    fix new users lol 3

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -3,6 +3,6 @@ import type { PageServerLoad } from './$types'
 
 export const load: PageServerLoad = async ({ parent, cookies }) => {
 	const { task_list } = await parent()
-	if (!task_list) return {}
-	redirect(302, '/' + (cookies.get('last_list_id') || task_list[0]?.id || ''))
+	if (!task_list?.length) return {}
+	redirect(302, '/' + (cookies.get('last_list_id') || task_list[0].id))
 }
