233451927af4116cb8d59d8f6712a7e2044bf8e5
Antonio Sarcevic <sarcevicantonio@gmail.com>
2025-12-30 13:30:15 +0100

    update packages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import Icons from 'unplugin-icons/vite'
+import devtoolsJson from 'vite-plugin-devtools-json'
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [
+		devtoolsJson(),
 		sveltekit(),
 		Icons({
 			compiler: 'svelte'
